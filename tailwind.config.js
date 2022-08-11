cecd214ead6e0c39441232c784840710323de154
Eric Afes <eriicafes@gmail.com>
2022-08-11 19:08:35 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        product: {
+          coral: "#EAF7FB",
+          rose: "#FEE5E4",
+          beige: "#FFF1E3",
+          purple: "#F2E4FD",
+          deepBlue: "#091572",
+          lightBlue: "#0200B6",
+          orange: "#FF9200",
+        }
+      }
+    },
   },
   plugins: [],
 }
